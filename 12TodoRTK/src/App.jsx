294db6e12fc8a1a4cd411ba5b7c769d19c1eb6f9
Bhuvanesh Jagtap <deshmukhkanha25@gmail.com>
2025-10-23 15:42:41 +0530

    TodoRTX

--- 12TodoRTK/src/App.jsx
+++ 12TodoRTK/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AddTodo from "./components/addTodo"
+import Todos from "./components/Todos"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Todos/>
+      <AddTodo/>
     </>
   )
 }
